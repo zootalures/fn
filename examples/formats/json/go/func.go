ba8cde8e932c30454ca1f1b23752db588e3cb2f2
amykang2020 <amykang2020@yahoo.com>
2017-09-06 02:10:28 -0700

    fix json-format example stdout marshaling

--- examples/formats/json/go/func.go
+++ examples/formats/json/go/func.go
@@ -12,8 +12,8 @@ type Person struct {
 }
 
 func (a *Person) UnmarshalJSON(b []byte) error {
-        a.Name = string(b)
-        return nil
+	a.Name = string(b)
+	return nil
 }
 
 type JSONInput struct {
@@ -23,23 +23,41 @@ type JSONInput struct {
 	Body       Person `json:"body"`
 }
 
+func (a *JSONInput) String() string {
+	return fmt.Sprintf("request_url=%s\ncall_id=%s\nmethod=%s\n\nbody=%s",
+		a.RequestURL, a.CallID, a.Method, a.Body)
+}
+
+type JSONOutput struct {
+	Body string `json:"body"`
+}
+
 func main() {
 	// p := &Person{Name: "World"}
 	// json.Unmarshal(os.Stdin).Decode(p)
 	// mapD := map[string]string{"message": fmt.Sprintf("Hello %s", p.Name)}
 	// mapB, _ := json.Marshal(mapD)
 	// fmt.Println(string(mapB))
+	log.Println("IN func.go")
 
 	dec := json.NewDecoder(os.Stdin)
 	enc := json.NewEncoder(os.Stdout)
 	for {
-		in := &JSONInput{Body: Person{Name: "World"}}
+		//in := &JSONInput{Body: Person{Name: "World"}}
+		in := &JSONInput{}
 		if err := dec.Decode(in); err != nil {
 			log.Fatalln(err)
 			return
 		}
+		in.Body = Person{Name: "World"}
+
+		log.Println(in)
+
 		mapResult := map[string]string{"message": fmt.Sprintf("Hello %s", in.Body.Name)}
-		if err := enc.Encode(mapResult); err != nil {
+		out := &JSONOutput{}
+		b, _ := json.Marshal(mapResult)
+		out.Body = string(b)
+		if err := enc.Encode(out); err != nil {
 			log.Fatalln(err)
 		}
 	}
