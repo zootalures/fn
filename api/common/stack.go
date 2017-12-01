f958abee1ec52c43031c1a4fc4839511c3c5a8f2
Reed Allman <rdallman10@gmail.com>
2017-12-01 07:29:25 -0800

    meh meh meh

--- api/common/stack.go
+++ api/common/stack.go
@@ -1,35 +1,48 @@
 package common
 
 type stack struct {
-	inbox chan chan int
+	inbox  chan chan int
 	outbox chan chan int
-}
 
-func newStack() *stack {
 	// stack of inbox items
 	list []chan int
 }
 
-func (s *stack) run() {
-	for {
-	// outbox is users requesting from the stack (they send us a channel to send to)
-	select {
-	case ch := <-outbox:
-		send(ch)
-	case in := <-inbox:
-		s.list = append(s.list, in)
+func newStack() *stack {
+	return &stack{
+		inbox:  make(chan chan int),
+		outbox: make(chan chan int),
 	}
 }
 
-func (s *stack) send(ch chan int) {
-			for len(s.list) > 0 {
-				in := s.list[len(s.list)-1]
-				s.list = s.list[:len(s.list)-1]
-					select {
-					case i, ok := <- in:
-						if ok {
-						}
-					default:
-				}
+func (s *stack) run() {
+	for {
+		// outbox is users requesting from the stack (they send us a channel to send to)
+		select {
+		case ch := <-s.outbox:
+			s.pop(ch)
+		case in := <-s.inbox:
+			s.list = append(s.list, in)
 		}
+	}
+}
+
+func (s *stack) pop(ch chan int) bool {
+	for len(s.list) > 0 {
+		in := s.list[len(s.list)-1]
+		s.list = s.list[:len(s.list)-1]
+		select {
+		case i, ok := <-in:
+			if ok {
+				select {
+				case ch <- i:
+				default:
+					// if they hung up, force them to re-submit
+				}
+				return true
+			}
+		default:
+		}
+	}
+	return false
 }
