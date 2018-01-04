d81903549528bb4d85c32c466a8d5ed323161340
Reed Allman <rdallman10@gmail.com>
2018-01-04 11:59:33 -0800

    explore simple channel based stack more

--- api/common/stack.go
+++ api/common/stack.go
@@ -1,48 +1,105 @@
 package common
 
-type stack struct {
-	inbox  chan chan int
-	outbox chan chan int
+import (
+	"context"
+	"sync"
+)
 
-	// stack of inbox items
-	list []chan int
+type Stack interface {
+	Push() chan<- interface{}
+	Pop() <-chan interface{}
+}
+
+type stack struct {
+	cond *sync.Cond
+	list []item
+}
+
+type item struct {
+	ch   <-chan interface{}
+	done <-chan struct{}
 }
 
 func newStack() *stack {
 	return &stack{
-		inbox:  make(chan chan int),
-		outbox: make(chan chan int),
+		cond: sync.NewCond(new(sync.Mutex)),
 	}
 }
 
-func (s *stack) run() {
+func (s *stack) Push(ctx context.Context) chan<- interface{} {
+	ch := make(chan interface{})
+	s.cond.L.Lock()
+	s.list = append(s.list, item{ch: ch, done: ctx.Done()})
+	s.cond.L.Unlock()
+	s.cond.Broadcast()
+	return ch
+}
+
+func (s *stack) Pop(ctx context.Context) <-chan interface{} {
+	ch := make(chan interface{})
+
+	go s.pop(ctx, ch)
+
+	return ch
+}
+
+// TODO play wit dis?
+//func (s *stack) popCh(ctx context.Context) <-chan <-chan interface{} {
+//ch := make(chan <-chan interface{})
+
+//go s.pop2(ctx, ch)
+
+//return ch
+//}
+
+func (s *stack) pop(ctx context.Context, ch chan<- interface{}) {
+	s.cond.L.Lock()
+	defer s.cond.L.Unlock()
+
 	for {
-		// outbox is users requesting from the stack (they send us a channel to send to)
-		select {
-		case ch := <-s.outbox:
-			s.pop(ch)
-		case in := <-s.inbox:
-			s.list = append(s.list, in)
+		for len(s.list) == 0 && ctx.Err() == nil {
+			s.cond.Wait()
 		}
-	}
-}
 
-func (s *stack) pop(ch chan int) bool {
-	for len(s.list) > 0 {
-		in := s.list[len(s.list)-1]
-		s.list = s.list[:len(s.list)-1]
-		select {
-		case i, ok := <-in:
-			if ok {
-				select {
-				case ch <- i:
-				default:
-					// if they hung up, force them to re-submit
-				}
-				return true
+		// TODO clean up should probably be push driven, if poppers dissipate there are still pushers
+
+		// walk from head to tail, remove any goners
+		for i := 0; i < len(s.list); i++ {
+			select {
+			case <-s.list[i].done:
+				s.list = append(s.list[:i], s.list[i+1:]...)
+				i--
+			default:
+				break // since this is LIFO this shouldn't be sparse, but we'll double check below
 			}
-		default:
 		}
+
+		// after clean up, if ctx is over, just bail
+		if ctx.Err() != nil {
+			return
+		}
+
+		// now walk from tail to head until somebody is there (should be tail, if anyone)
+		for i := len(s.list) - 1; i > -1; i-- {
+			in := s.list[i]
+			s.list = s.list[:i]
+
+			// NOTE we can't atomically receive / send (ch <- <-in.ch), deadlocks if sender on in.ch is gone
+			select {
+			default: // loop again
+			case <-ctx.Done(): // XXX (reed): could do this before checking in.ch, which may beat this (it's random)
+				return
+			case it := <-in.ch:
+				select {
+				default:
+					// XXX (reed): our use case needs to call it.Done() here if popper is gone but we recvd from a pusher.
+					// see possible ideas around popCh?
+				case ch <- it:
+				}
+				return
+			}
+		}
+
+		// cond is still locked here
 	}
-	return false
 }
