89657a44d5330077b88c9d18a1cb352065d84e68
Reed Allman <rdallman10@gmail.com>
2018-01-05 01:49:50 -0800

    test properly fails now

--- api/agent/agent_test.go
+++ api/agent/agent_test.go
@@ -1,13 +1,16 @@
 package agent
 
 import (
+	"bufio"
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"strconv"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -16,6 +19,17 @@ import (
 	"github.com/fnproject/fn/api/mqs"
 	"github.com/sirupsen/logrus"
 )
+
+func init() {
+	// TODO figure out some sane place to stick this
+	formatter := &logrus.TextFormatter{
+		FullTimestamp: true,
+	}
+	logrus.SetFormatter(formatter)
+	logrus.SetLevel(logrus.DebugLevel)
+}
+
+// TODO need to add at least one test for our cachy cache
 
 func checkExpectedHeaders(t *testing.T, expectedHeaders http.Header, receivedHeaders http.Header) {
 
@@ -76,7 +90,7 @@ func TestCallConfigurationRequest(t *testing.T) {
 		}, nil,
 	)
 
-	a := New(NewCachedDataAccess(NewDirectDataAccess(ds, ds, new(mqs.Mock))))
+	a := New(NewDirectDataAccess(ds, ds, new(mqs.Mock)))
 	defer a.Close()
 
 	w := httptest.NewRecorder()
@@ -185,7 +199,7 @@ func TestCallConfigurationModel(t *testing.T) {
 	const timeout = 1
 	const idleTimeout = 20
 	const memory = 256
-	CPUs := models.MilliCPUs(1000)
+	// CPUs := models.MilliCPUs(1000)
 	method := "GET"
 	url := "http://127.0.0.1:8080/r/" + appName + path
 	payload := "payload"
@@ -196,10 +210,10 @@ func TestCallConfigurationModel(t *testing.T) {
 		"FN_APP_NAME": appName,
 		"FN_PATH":     path,
 		"FN_MEMORY":   strconv.Itoa(memory),
-		"FN_CPUS":     CPUs.String(),
-		"FN_TYPE":     typ,
-		"APP_VAR":     "FOO",
-		"ROUTE_VAR":   "BAR",
+		// "FN_CPUS":     CPUs.String(),
+		"FN_TYPE":   typ,
+		"APP_VAR":   "FOO",
+		"ROUTE_VAR": "BAR",
 	}
 
 	cm := &models.Call{
@@ -212,16 +226,16 @@ func TestCallConfigurationModel(t *testing.T) {
 		Timeout:     timeout,
 		IdleTimeout: idleTimeout,
 		Memory:      memory,
-		CPUs:        CPUs,
-		Payload:     payload,
-		URL:         url,
-		Method:      method,
+		// CPUs:        CPUs,
+		Payload: payload,
+		URL:     url,
+		Method:  method,
 	}
 
 	// FromModel doesn't need a datastore, for now...
 	ds := datastore.NewMockInit(nil, nil, nil)
 
-	a := New(NewCachedDataAccess(NewDirectDataAccess(ds, ds, new(mqs.Mock))))
+	a := New(NewDirectDataAccess(ds, ds, new(mqs.Mock)))
 	defer a.Close()
 
 	callI, err := a.GetCall(FromModel(cm))
@@ -291,7 +305,7 @@ func TestAsyncCallHeaders(t *testing.T) {
 	// FromModel doesn't need a datastore, for now...
 	ds := datastore.NewMockInit(nil, nil, nil)
 
-	a := New(NewCachedDataAccess(NewDirectDataAccess(ds, ds, new(mqs.Mock))))
+	a := New(NewDirectDataAccess(ds, ds, new(mqs.Mock)))
 	defer a.Close()
 
 	callI, err := a.GetCall(FromModel(cm))
@@ -386,7 +400,7 @@ func TestSubmitError(t *testing.T) {
 	// FromModel doesn't need a datastore, for now...
 	ds := datastore.NewMockInit(nil, nil, nil)
 
-	a := New(NewCachedDataAccess(NewDirectDataAccess(ds, ds, new(mqs.Mock))))
+	a := New(NewDirectDataAccess(ds, ds, new(mqs.Mock)))
 	defer a.Close()
 
 	callI, err := a.GetCall(FromModel(cm))
@@ -406,4 +420,195 @@ func TestSubmitError(t *testing.T) {
 	if cm.Error == "" {
 		t.Fatal("expected error string to be set on call")
 	}
+}
+
+// return a model with all fields filled in with fnproject/sleeper image, change as needed
+func testCall() *models.Call {
+	appName := "myapp"
+	path := "/sleeper"
+	image := "fnproject/fn-test-utils"
+	const timeout = 10
+	const idleTimeout = 20
+	const memory = 256
+	CPUs := models.MilliCPUs(200)
+	method := "GET"
+	url := "http://127.0.0.1:8080/r/" + appName + path
+	payload := "payload"
+	typ := "async"
+	format := "http"
+	contentType := "suberb_type"
+	contentLength := strconv.FormatInt(int64(len(payload)), 10)
+	config := map[string]string{
+		"FN_FORMAT":   format,
+		"FN_APP_NAME": appName,
+		"FN_PATH":     path,
+		"FN_MEMORY":   strconv.Itoa(memory),
+		"FN_CPUS":     CPUs.String(),
+		"FN_TYPE":     typ,
+		"APP_VAR":     "FOO",
+		"ROUTE_VAR":   "BAR",
+		"DOUBLE_VAR":  "BIZ, BAZ",
+	}
+	headers := map[string][]string{
+		// FromRequest would insert these from original HTTP request
+		"Content-Type":   []string{contentType},
+		"Content-Length": []string{contentLength},
+	}
+
+	return &models.Call{
+		Config:      config,
+		Headers:     headers,
+		AppName:     appName,
+		Path:        path,
+		Image:       image,
+		Type:        typ,
+		Format:      format,
+		Timeout:     timeout,
+		IdleTimeout: idleTimeout,
+		Memory:      memory,
+		CPUs:        CPUs,
+		Payload:     payload,
+		URL:         url,
+		Method:      method,
+	}
+}
+
+func TestPipesAreClear(t *testing.T) {
+	// The basic idea here is to make a call start a hot container, and the
+	// first call has a reader that only reads after a delay, which is beyond
+	// the boundary of the first call's timeout. Then, run a second call
+	// with a different body that also has a delay, in which time the first
+	// call's reader is expected to read before the second's. make sure the
+	// second call does not get the first call's body. This ensures the input
+	// paths for calls do not overlap into the same container.
+	// TODO for testing output we need to make sure the first call's writer doesn't get anything? think moar
+	// TODO need to consider timings further, may be finicky in constrained environments (time for mock timings?)
+	//
+	// causal (seconds):
+	// T1=start task one, T1TO=task one times out, T2=start task two
+	// T1W=task one writes, T2W=task two writes
+	//
+	//
+	//  1s  2   3    4   5   6
+	// ---------------------------
+	//
+	// T1-------T1TO-T2--T1W-T2W
+
+	// TODO echo image
+	call := testCall()
+	call.Type = "sync"
+	call.Format = "http"
+	call.IdleTimeout = 60 // keep this bad boy alive
+	call.Timeout = 4      // short
+
+	// we need to load in app & route so that FromRequest works
+	ds := datastore.NewMockInit(
+		[]*models.App{
+			{Name: call.AppName},
+		},
+		[]*models.Route{
+			{
+				Path:        call.Path,
+				AppName:     call.AppName,
+				Image:       call.Image,
+				Type:        call.Type,
+				Format:      call.Format,
+				Timeout:     call.Timeout,
+				IdleTimeout: call.IdleTimeout,
+				Memory:      call.Memory,
+			},
+		}, nil,
+	)
+
+	a := New(NewDirectDataAccess(ds, ds, new(mqs.Mock)))
+	defer a.Close()
+
+	// grab a buffer so we can read what gets written to this guy
+	// use same output for both, to make sure we don't see the first response in there.
+	// TODO shouldn't we make 2 and then check both after the 2nd completes instead of intermingling?
+	var outtie bytes.Buffer
+
+	// read this body after 5s (after call times out)
+	bodOne := `{"echoContent":"yodawg"}`
+	delayBodyOne := &delayReader{inny: strings.NewReader(bodOne), delay: 5 * time.Second}
+
+	req, err := http.NewRequest("GET", call.URL, delayBodyOne)
+	if err != nil {
+		t.Fatal("unexpected error building request", err)
+	}
+
+	callI, err := a.GetCall(FromRequest(call.AppName, call.Path, req), WithWriter(&outtie))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// this will time out after 4s, our reader reads after 5s
+	t.Log("before submit one:", time.Now())
+	err = a.Submit(callI)
+	t.Log("after submit one:", time.Now())
+	if err == nil {
+		t.Error("expected error but got none")
+	}
+
+	// TODO we need to make sure we got an http parsing error here (and that it's user friendly for fdks?)
+
+	// if we submit the same call to the hot container again,
+	// this can be finicky if the
+	// hot logic simply fails to re-use a container then this will
+	// 'just work' but at one point this failed.
+
+	// only delay this body 2 seconds, so that we read at 6s (first writes at 5s) before time out
+	bodTwo := `{"echoContent":"NODAWG"}`
+	delayBodyTwo := &delayReader{inny: strings.NewReader(bodTwo), delay: 2 * time.Second}
+
+	req, err = http.NewRequest("GET", call.URL, delayBodyTwo)
+	if err != nil {
+		t.Fatal("unexpected error building request", err)
+	}
+
+	callI, err = a.GetCall(FromRequest(call.AppName, call.Path, req), WithWriter(&outtie))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("before submit two:", time.Now())
+	err = a.Submit(callI)
+	t.Log("after submit two:", time.Now())
+	if err != nil {
+		// NOTE: this shouldn't error here, but it may because the first call reader gets read
+		// during this call's duration. don't do a Fatal so that we can read the body to see
+		// what really happened
+		t.Error("got error from submit when task should succeed")
+	}
+
+	// we're using http format so this will have written a whole http request
+	res, err := http.ReadResponse(bufio.NewReader(&outtie), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	// {"request":{},"header":{"Fn_call_id":["01C32SA8WX0000600000000000"]
+	var resp struct {
+		H http.Header `json:"header"`
+	}
+
+	json.NewDecoder(res.Body).Decode(&resp)
+
+	rid := resp.H.Get("FN_CALL_ID")
+
+	if rid != callI.Model().ID {
+		t.Fatalf("body from second call was not what we wanted. boo. got wrong id: %v wanted: %v", rid, callI.Model().ID)
+	}
+}
+
+type delayReader struct {
+	once  sync.Once
+	inny  io.Reader
+	delay time.Duration
+}
+
+func (r *delayReader) Read(b []byte) (int, error) {
+	r.once.Do(func() { time.Sleep(r.delay) })
+	return r.inny.Read(b)
 }
